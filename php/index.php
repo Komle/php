1a9e866603667a5b5fc78a361db0b60f3c488f8c
Komle <deizyra@hotmail.lt>
2020-09-02 10:11:12 +0300

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -1,1 +1,12 @@
-?>php
+<?php
+
+$name = "Jonas";
+$lastname = "Jonaitis";
+$amzius = "39";
+$pomegiai = ['Programuoti', 'Zvejoti', 'Keliauti'];
+
+echo "Mokinio duomenys:<br>";
+echo "Vardas: {$name}<br>";
+echo "Pavarde: {$lastname}<br>";
+echo "Amzius: {$amzius}<br>";
+echo "Pomegiai:<br> {$pomegiai[0]}<br> {$pomegiai[1]}<br> {$pomegiai[2]}<br>";
