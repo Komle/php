dfa0912bb125c174d0f02caa0138253fb6a58950
Komle <deizyra@hotmail.lt>
2020-09-02 13:59:03 +0300

    Add files via upload

--- php/data.php
+++ php/data.php
@@ -2,6 +2,53 @@
 $name = "Jonas";
 $lastname = "Jonaitis";
 $amzius = "39";
-$pomegiai = ['Programuoti', 'Zvejoti', 'Keliauti'];
-
+$pomegiai = [];
+$pomegiai = [
+    0=>'Programuoti',
+    1=>'Zvejoti',
+    2=>'Keliauti'
+];
+$data = [
+    'Komp-1'=>'MacBook',
+    'Phone-15'=>'Iphone',
+    'Job' =>'Developer'
+];   
+$workers = [
+    'IT' =>[
+        'Jonas',
+        'Andrius',
+        'Karolis'
+    ],
+    'ADMIN' =>[
+        'Ieva',
+        'Simona',
+        'Irma'
+    ]
+];
 $total = 1;
+
+$counter = count($data);
+
+
+/* For ciklas
+for ($i=0; $i<$counter;$i++){
+    echo "$data[$i]<br>"*/
+
+ /*   foreach($data as $id => $student){
+        echo "$id:$student";
+    }
+    foreach($workers as $id => $workeriai){
+        echo "$id$workeriai";
+    */   
+?>
+<ul>
+<?php foreach($workers as $id => $workeriai):?>
+    <li><?=$id;?>
+    <ul>
+    <?php foreach($workers[$id] as $workeriai):?>
+    <li><?=$workeriai;?></li>
+    <?php endforeach ?>
+    </ul>
+    </li>
+<?php endforeach ?>
+</ul>
--- php/index.view.php
+++ php/index.view.php
@@ -1,6 +1,33 @@
 <?php
+include 'data.php';
 echo "Mokinio duomenys:<br>";
 echo "Vardas: {$name}<br>";
 echo "Pavarde: {$lastname}<br>";
 echo "Amzius: {$amzius}<br>";
 echo "Pomegiai:<br> {$pomegiai[0]}<br> {$pomegiai[1]}<br> {$pomegiai[2]}<br>";
+
+if($total > 18){
+    echo "Pirma vieta<br>";
+} else if($total >12){
+    echo "Antra vieta<br>";
+} else if($total >10){
+    echo "Trecia vieta<br>";
+}
+else{
+    echo "Aciu, kad dalyvavote<br>";
+}
+
+switch($total){
+    case 18:
+        echo "Pirma vieta<br>";
+    break;
+        case 15:
+            echo "Antra vieta<br>";
+        break;
+        case 10:
+            echo "Trecia vieta<br>";
+        break;
+        default:
+        echo "Aciu kad dalyvavote<br>";
+
+}
